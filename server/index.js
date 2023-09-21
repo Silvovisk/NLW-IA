f635528ff59c53c0be77d9791308caab478fb409
Silvia Vasconcelos <vascsilvia@gmail.com>
2023-09-21 16:35:00 -0300

    Construçao do back-end

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import cors from "cors"
+import express from "express"
+
+import { download } from "./download.js"
+
+const app = express()
+app.use(cors())
+
+// utilizar o metodo get, definir o recurso - endereço da rota(summary), utilizar parametro para receber o id, criou uma função de download
+app.get("/summary/:id", (request, response) => {
+  download(request.params.id)
+  response.json({ result: "Download do vídeo realizado com sucesso!" })
+})
+
+// iniciar o servidor, definindo o endereço da porta
+app.listen(3333, () => console.log("Server is running on port 3333"))
